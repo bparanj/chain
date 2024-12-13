f2edf8bffc54a0d332f17f51a13655b7bab2270b
Bala Paranj <bparanj@gmail.com>
2024-12-13 12:34:19 -0500

    Upgrade deeplearning.ai example code to latest version

---
+++ chatapi/style.py
@@ -0,0 +1,38 @@
+import openai
+from langchain_openai import OpenAI
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from creds import API_KEY
+
+openai.api_key = API_KEY
+
+customer_email = """
+Arrr, I be fuming that me blender lid \
+flew off and splattered me kitchen walls \
+with smoothie! And to make matters worse,\
+the warranty don't cover the cost of \
+cleaning up me kitchen. I need yer help \
+right now, matey!
+"""
+
+style = """American English \
+in a calm and respectful tone
+"""
+
+prompt = f"""Translate the text \
+that is delimited by triple backticks
+into a style that is {style}.
+text: ```{customer_email}```
+"""
+
+# Create an LLM instance from LangChain
+llm = OpenAI(temperature=0)
+
+# Generate a completion
+response = llm.invoke(prompt)
+
+print(response)
